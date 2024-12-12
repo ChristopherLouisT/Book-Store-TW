d8ffdae70835d98b5f0e50e5935911982e0932a7
Ein <142294432+ChristopherLouisT@users.noreply.github.com>
2024-12-12 18:06:48 +0700

    Update online_book_store_db.sql

--- online-book-store-main/online_book_store_db.sql
+++ online-book-store-main/online_book_store_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2024 at 10:02 AM
+-- Generation Time: Dec 12, 2024 at 12:06 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,7 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `full_name`, `email`, `password`) VALUES
-(1, 'Elias', 'eliasfsdev@gmail.com', '123456');
+(1, 'Elias', 'eliasfsdev@gmail.com', '$2y$10$ihbRHP3cjetzIzZohrTdPOaE8Orrkrgqm6P5f1lFkAWVVXBXLUr0m');
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,8 @@ INSERT INTO `authors` (`id`, `name`) VALUES
 (3, 'Harper Lee'),
 (4, 'J.K. Rowling'),
 (5, 'George R.R. Martin'),
-(6, 'Agatha Christie');
+(6, 'Agatha Christie'),
+(7, 'Shiraishi Jougi');
 
 -- --------------------------------------------------------
 
@@ -86,7 +87,8 @@ CREATE TABLE `books` (
 
 INSERT INTO `books` (`id`, `title`, `author_id`, `description`, `category_id`, `cover`, `file`) VALUES
 (1, 'Pride and Prejudice', 1, 'Sparks fly when spirited Elizabeth Bennet meets single, rich, and proud Mr. Darcy. But Mr. Darcy reluctantly finds himself falling in love with a woman beneath his class. Can each overcome their own pride and prejudice?', 8, 'prideandprejustice.jpeg', 'Soal Tree.pdf'),
-(2, '1984', 2, 'In 1984, civilisation has been ravaged by world war, civil conflict, and revolution. Airstrip One (formerly known as Great Britain) is a province of Oceania, one of the three totalitarian super-states that rule the world. It is ruled by \"The Party\" under the ideology of \"Ingsoc\" (a Newspeak shortening of \"English Socialism\") and the mysterious leader Big Brother, who has an intense cult of personality. The Party brutally purges out anyone who does not fully conform to their regime, using the Thought Police and constant surveillance through telescreens (two-way televisions), cameras, and hidden microphones. Those who fall out of favour with the Party become \"unpersons\", disappearing with all evidence of their existence destroyed.\r\nIn London, Winston Smith is a member of the Outer Party, working at the Ministry of Truth, where he rewrites historical records to conform to the state\'s ever-changing version of history. Winston revises past editions of The Times, while the original documents are destroyed after being dropped into ducts known as memory holes, which lead to an immense furnace. He secretly opposes the Party\'s rule and dreams of rebellion, despite knowing that he is already a \"thought-criminal\" and is likely to be caught one day.\r\n“Doublethink means the power of holding two contradictory beliefs in one\'s mind simultaneously, and accepting both of them.”', 8, '1984.jpg', 'DDL.pdf');
+(2, '1984', 2, 'In 1984, civilisation has been ravaged by world war, civil conflict, and revolution. Airstrip One (formerly known as Great Britain) is a province of Oceania, one of the three totalitarian super-states that rule the world. It is ruled by \"The Party\" under the ideology of \"Ingsoc\" (a Newspeak shortening of \"English Socialism\") and the mysterious leader Big Brother, who has an intense cult of personality. The Party brutally purges out anyone who does not fully conform to their regime, using the Thought Police and constant surveillance through telescreens (two-way televisions), cameras, and hidden microphones. Those who fall out of favour with the Party become \"unpersons\", disappearing with all evidence of their existence destroyed.\r\nIn London, Winston Smith is a member of the Outer Party, working at the Ministry of Truth, where he rewrites historical records to conform to the state\'s ever-changing version of history. Winston revises past editions of The Times, while the original documents are destroyed after being dropped into ducts known as memory holes, which lead to an immense furnace. He secretly opposes the Party\'s rule and dreams of rebellion, despite knowing that he is already a \"thought-criminal\" and is likely to be caught one day.\r\n“Doublethink means the power of holding two contradictory beliefs in one\'s mind simultaneously, and accepting both of them.”', 8, '1984.jpg', 'DDL.pdf'),
+(4, 'The Journey Of Elaina', 7, 'Since childhood, Elaina has always been fascinated by the stories written within her favorite book, especially those about Nike, a renowned witch who had numerous great travels across the world. Wanting to experience the awe of adventure herself, Elaina strives to become a witch, and despite the numerous trials that come her way, she eventually succeeds.  Now a full-fledged witch, Elaina finally embarks on her long-awaited journey, in which she meets many people along the way, learning their various stories. Through all of this, she explores the world at its fullest—experiencing both its bright and dark sides—starting her legendary tale', 3, '675005007d2f50.28007394.png', '675005007d8195.35786717.pdf');
 
 -- --------------------------------------------------------
 
@@ -144,7 +146,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `full_name`) VALUES
-(1, 'bdstwn@gmail.com', '12345', 'Budi Setiawan');
+(1, 'bdstwn@gmail.com', '$2y$10$DNatemfUfLz9KjhTweX6deFYrWQrcb8qA.OMMDo35GLDTmATYgomO', 'Budi Setiawan');
 
 --
 -- Indexes for dumped tables
@@ -200,13 +202,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `buybook`
