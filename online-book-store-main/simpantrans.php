26ed711dd863dbd48d4626cf72c87e53f44938b8
Joice <142294645+valmosee@users.noreply.github.com>
2024-12-13 10:47:22 +0700

    minor

--- online-book-store-main/simpantrans.php
+++ online-book-store-main/simpantrans.php
@@ -3,7 +3,7 @@ session_start();
 include "db_conn.php";
 
 $idbook = $_POST['idbook']; 
-$iduser = 99; 
+$iduser = $_SESSION['user_id'] = $user_id;; 
 
 $stmt = $conn->prepare("select * from buybook where user_id = ? and book_id = ?");
 $stmt->execute([$iduser, $idbook]);
