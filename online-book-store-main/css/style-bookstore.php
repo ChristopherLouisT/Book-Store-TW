40857b4602587fe148aed0fbbc0ed0297f873146
Yoo <142294379+yoochann@users.noreply.github.com>
2024-12-15 20:24:28 +0700

    Update style-bookstore.php

--- online-book-store-main/css/style-bookstore.php
+++ online-book-store-main/css/style-bookstore.php
@@ -1,11 +1,43 @@
 <link href="css/Bookstore.css" rel="stylesheet" type = "text/css">
 
 <style>
+/* Card img size */
 .card-img-top {
     height: 350px;
     object-fit: cover;
     border-radius: 5px;
 }
+
+/* Dark mode */
+body.dark-mode {
+    background-color: #121212;
+    color: #ffffff;
+}
+
+.navbar.dark-mode {
+    background-color: #333333 !important;
+}
+
+.card.dark-mode {
+    background-color: #1e1e1e;
+    color: #ffffff;
+    border-color: #444444;
+}
+
+.list-group-item.dark-mode {
+    background-color: #1e1e1e;
+    color: #ffffff;
+}
+
+.book-popup.dark-mode {
+    background: rgba(0, 0, 0, 0.8);
+}
+
+.btn-outline-light.dark-mode {
+    border-color: #ffffff;
+    color: #ffffff;
+}
+
 
 /* Popup Styles index-type-category-author-search */
 .book-popup {
