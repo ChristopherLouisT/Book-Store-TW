48bd8c37e2058ab46d1798dddbb6ed8c492611a2
Ein <142294432+ChristopherLouisT@users.noreply.github.com>
2024-12-20 10:32:00 +0700

    Final Add Author

--- online-book-store-main/add-author.php
+++ online-book-store-main/add-author.php
@@ -5,68 +5,49 @@ session_start();
 if (isset($_SESSION['user_id']) &&
     isset($_SESSION['user_email'])) {
 
-	# CSS Helper Function
-	include "css/style-bookstore.php";
-?>
+	# Database Connection File
+	include "../db_conn.php";
+	$cnt = new Connection();
+	$conn = $cnt->getConn();
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Add Author</title>
 
-    <!-- bootstrap 5 CDN-->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+	if (isset($_POST['author_name'])) {
+		$name = $_POST['author_name'];
 
-    <!-- bootstrap 5 Js bundle CDN-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+		#simple form Validation
+		if (empty($name)) {
+			$em = "The author name is required";
+			header("Location: ../add-author.php?error=$em");
+            exit;
+		}else {
+			# Insert Into Database
+			$sql  = "INSERT INTO authors (name)
+			         VALUES (?)";
+			$stmt = $conn->prepare($sql);
+			$res  = $stmt->execute([$name]);
 
-</head>
-<body>
-	<div class="container">
-		<div class = "row">
-			<?php include("menunavbarAdmin.php"); ?>
-		</div>
+			/**
+		      If there is no error while 
+		      inserting the data
+		    **/
+		     if ($res) {
+		     	# success message
+		     	$sm = "Successfully created!";
+				header("Location: ../add-author.php?success=$sm");
+	            exit;
+		     }else{
+		     	# Error message
+		     	$em = "Unknown Error Occurred!";
+				header("Location: ../add-author.php?error=$em");
+	            exit;
+		     }
+		}
+	}else {
+      header("Location: ../admin.php");
+      exit;
+	}
 
-		<div class = "row g-0 mt-5">
-			<form action="php/add-author.php"
-				method="post" 
-				class="shadow p-4 rounded mt-5"
-				style="width: 90%; max-width: 50rem;">
-
-				<h1 class="text-center pb-5 display-4 fs-3">
-					Add New Author
-				</h1>
-				<?php if (isset($_GET['error'])) { ?>
-				<div class="alert alert-danger" role="alert">
-					<?=htmlspecialchars($_GET['error']); ?>
-				</div>
-				<?php } ?>
-				<?php if (isset($_GET['success'])) { ?>
-				<div class="alert alert-success" role="alert">
-					<?=htmlspecialchars($_GET['success']); ?>
-				</div>
-				<?php } ?>
-				<div class="mb-3">
-					<label class="form-label">
-							Author Name
-						</label>
-					<input type="text" 
-						class="form-control" 
-						name="author_name">
-				</div>
-
-				<button type="submit" 
-						class="btn btn-primary">
-						Add Author</button>
-			</form>
-		</div>
-	</div>
-</body>
-</html>
-
-<?php }else{
-  header("Location: login.php");
+}else{
+  header("Location: ../login.php");
   exit;
-} ?>
+}
